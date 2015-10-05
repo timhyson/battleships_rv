e225f6f79acab3cc4ccd83541b5869b264e9be28
Tim Hyson <tjhyson@gmail.com>
2015-10-05 13:39:14 +0100

    ship can be hit and can be sunk

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,20 +1,21 @@
 class Ship
 
-  attr_reader :position, :hits
+  attr_reader :position, :hits, :size
 
-  def initialize(position)
+  def initialize(position, size = 1)
     @position = position
     @hits     = []
+    @size     = size
   end
-#   def initialize(size = 1)
-#     @size = size
-#     @health = size
-#   end
 
   def hit(position)
     return :miss unless self.position.include?(position)
     hits << position unless hits.include?(position)
     :hit
+  end
+
+  def sunk?
+    @hits.size >= @size
   end
 
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -11,6 +11,29 @@ describe Ship do
     expect(ship.hit('A1')).to be(:hit)
   end
 
+  it 'will record number of hits' do
+    ship.hit('A1')
+    expect(ship.hits).to eq(['A1'])
+  end
+
+  it 'can be missed' do
+    expect(ship.hit('B1')).to eq(:miss)
+  end
+
+  it 'will not be hit when it is missed' do
+    ship.hit('B1')
+    expect(ship.hits).to eq([])
+  end
+
+  it 'is not sunk initially' do
+    expect(ship).not_to be_sunk
+  end
+
+  it 'can be sunk' do
+    ship.hit('A1')
+    expect(ship).to be_sunk
+  end
+
 #   let(:subject){Ship.new(3)}
 #
 #   it 'ship has health which is equal its life' do
