865f0124a0e37c21ce4d5d9592281eb348ffbe14
Tim Hyson <tjhyson@gmail.com>
2015-10-05 14:59:00 +0100

    player can report misses

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -22,5 +22,17 @@ describe Player do
     subject.receive_hit('B2')
     expect(subject.hits).to eq(['A1', 'B2'])
   end
-  
+
+  it 'can report miss positions' do
+    subject.receive_hit('A1')
+    subject.receive_hit('B2')
+    expect(subject.misses).to eq(['A1', 'B2'])
+  end
+
+  it 'will lose if all ships are hit' do
+    subject.place(Ship, 'A1')
+    subject.receive_hit('A1')
+    expect(subject.lost?).to be(true)
+  end
+
 end
